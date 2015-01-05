4ceedf2d6840e82869722bd965fb556f0e5f7838
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-05 12:49:46 +0000

    node server runs, listen port 8080

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+
+  //setup
+
+  var express = require('express');
+  var app     = express();
+
+  var mongoose = require('mongoose');
+  var morgan   = require('morgan');
+  var bodyParser = require('body-parser');
+  var methodOverride = require('method-override');
+
+  // config
+
+  mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uw03mypu');
+
+  app.use(express.static(__dirname + '/public'));
+  app.use(morgan('dev'));
+  app.use(bodyParser.urlencoded({'extended':'true'}));
+  app.use(bodyParser.json());
+  app.use(bodyParser.json({ type: 'application?vnd.api+json'}));
+  app.use(methodOverride());
+  app.listen(8080);
+  console.log("App listening on port 8080")
